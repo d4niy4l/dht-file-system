50a4f908e48b7e83474fc5114c6c4c5d02a397c2
iqra028 <152682510+iqra028@users.noreply.github.com>
2023-12-15 17:25:37 +0300

    Update utlity.h

--- utlity.h
+++ utlity.h
@@ -1,4 +1,18 @@
 #pragma once
+/************************************************************************
+TITLE: VECTOR
+DESCRIPTION:
+This class was made to serve as a
+replacement for the static array in C++,
+to efficiently grow the array and manage
+dynamic memory.
+REQUIREMENTS: COPY CONSTRUCTOR FOR THE TYPE MUST BE DEFINED
+AUTHOR: DANIYAL AHMED [22I-1032]
+COAUTHOR: MUHAMMAD AHMAD [22I-1056]
+COAUTHOR: HAFIZA IQRA [22I-1269]
+DATE: MONDAY 4-12-2023
+LASTEDITBY: MUHAMMAD AHMAD
+*************************************************************************/
 #include <iostream>
 #include <fstream>
 #include "BigInt.h"
